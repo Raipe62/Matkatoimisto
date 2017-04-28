20b59a6cc49ade7c069426146f049161aaf76c92
WWW-Ryhma13 <rauno.vesti@gmail.com>
2017-04-28 15:03:30 +0300

    Update 2017-04-28T12:03:29.076Z

---
+++ clone.js
@@ -0,0 +1,13 @@
+﻿
+var nodegit = require('nodegit'),
+    path = require('path');
+
+var url = "https://github.com/WWW-Ryhma13/Matkatoimisto.git",
+    local = "./arkisto",
+    cloneOpts = {};
+
+nodegit.Clone(url, local, cloneOpts).then(function (repo) {
+    console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
+}).catch(function (err) {
+    console.log(err);
+});
